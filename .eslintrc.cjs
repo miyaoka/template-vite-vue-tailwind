19aecd1f3a1fce7b91d2a23806eaa3dcd9eb254a
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-06 06:44:50 +0000

    add lint rules for import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,33 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
+  plugins: ["import", "unused-imports"],
+  rules: {
+    "no-console": "warn",
+    "vue/no-ref-as-operand": "error",
+    "vue/multi-word-component-names": "warn",
+
+    // unused-imports
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+    "import/no-duplicates": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+  },
 };
