f4bdd040d20083368089bb0b93b72127c5556335
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-12 02:15:05 +0000

    add mdi icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>main</main>
+  <main class="flex flex-col p-4">
+    <p>main page</p>
+    <i class="i-mdi-success outline bg-green-400 w-10 h-10" />
+  </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
+
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    iconsPlugin({
+      collections: getIconCollections(["mdi"]),
+    }),
+  ],
 };
