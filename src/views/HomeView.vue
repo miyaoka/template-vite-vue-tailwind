c816459f14c956c94e7f932cdc6a758a8b8996e9
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-26 04:41:57 +0000

    fix lint error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,6 @@
 <template>
   <main class="flex flex-col p-4">
     <p>main page</p>
-    <i class="i-mdi-success h-10 w-10 bg-green-400 outline" />
+    <i class="i-mdi-success size-10 bg-green-400 outline" />
   </main>
 </template>
